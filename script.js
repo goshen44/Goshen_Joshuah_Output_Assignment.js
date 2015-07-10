7e357740a3bfd833389dd6ea2a0ad60c67bf9627
goshen44 <jpgoshen@gmail.com>
2015-07-09 23:16:45 -0400

    3rd Commit

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ var term= "Summer 2015";
 console.log(term);
 var assignment= "Creating Variables and Output Assignment";
 console.log(assignment);
-console.log("Years on the Planet")
+console.log("Years on the Planet");
 var yearsOnThePlanet=30;
 console.log(yearsOnThePlanet);
+console.log ("How old is my daughter");
+var myDaughterIsHowOld=8;
+console.log(myDaughterIsHowOld);
+console.log ("How many dogs do I have")
+var howManyDogsDoIHave=2
+console.log(howManyDogsDoIHave);
