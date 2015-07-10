c16051958b75572f2290e5ade60177b72c7f6f20
goshen44 <jpgoshen@gmail.com>
2015-07-09 22:47:15 -0400

    first repository

---
+++ script.js
@@ -0,0 +1,9 @@
+var firstAndLastName = "Josh Goshen";
+console.log(firstAndLastName);
+var classAndSection = "Scalable Data Infrastructures 1-Online, Section 01";
+console.log(classAndSection);
+var term= "Summer 2015";
+console.log(term);
+var assignment= "Creating Variables and Output Assignment";
+console.log(assignment);
+var yearsOnThePlanet=30;
